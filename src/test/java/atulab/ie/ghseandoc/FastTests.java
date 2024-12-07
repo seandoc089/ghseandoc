a7fa314f96a68f3c9d52bcff725692b741e42299
Sean Doherty <l00175352@atu.ie>
2024-12-07 18:29:34 +0000

    Test suite added

---
+++ src/test/java/atulab/ie/ghseandoc/FastTests.java
@@ -0,0 +1,10 @@
+package atulab.ie.ghseandoc;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({ AppTest.class, CarTest.class })
+public class FastTests {
+
+}
